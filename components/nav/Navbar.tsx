bf01bfdde8376f20021671dd41fb690692501a69
Deepak Mardi <deepakmardi22@gmail.com>
2024-02-13 19:07:26 +0530

    Layout Navbar

---
+++ components/nav/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+import { FaGithub } from "react-icons/fa";
+
+function Navbar() {
+  return (
+    <nav className="flex items-center justify-between">
+      <div className="group">
+        <Link href="/" className="text-2xl font-bold">
+          DevLog
+        </Link>
+        <div className="h-1 w-0 group-hover:w-full transition-all bg-violet-500"></div>
+      </div>
+      <Button variant="outline" className="flex items-center gap-2">
+        <FaGithub />
+        Login
+      </Button>
+    </nav>
+  );
+}
+
+export default Navbar;
